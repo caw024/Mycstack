e7dd749ddc412d0a99ddad9cdbcb0084125f821e
Calvin Aw <caw@stuy.edu>
2019-04-06 22:54:14 -0400

    alright

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ t = new_matrix()
 ident(t)
 csystems = [ t ]
 
-#parse_file( 'script', edges, polygons, csystems, screen, color )
+parse_file( 'script', edges, polygons, csystems, screen, color )
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 255 ]
 edges = []
 polygons = []
 t = new_matrix()
